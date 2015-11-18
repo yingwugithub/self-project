d2fc1252df3dda44820f9e52227ee4f68996742a
yingwugithub <3062557223@qq.com>
2015-11-18 14:34:36 +0800

    test

--- src/main/java/jdk/MacManage.java
+++ src/main/java/jdk/MacManage.java
@@ -3,4 +3,6 @@ package jdk;
 public interface MacManage {
 	public String operate( int type );
 	public String operater( int type );
+	public String operaterCheckout( int type );
+	public String operaterBranch( int type );
 }
